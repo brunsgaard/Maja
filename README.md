bf0422ee7715c126e128368ffb892740baaafeb3
Jonas Brunsgaard <jonas.brunsgaard@gmail.com>
2015-04-08 07:48:43 +0200

    CollecterClass added

--- README.md
+++ README.md
@@ -1,15 +1,42 @@
 # What is maja
 
-maja is a small application/script that fetches data from a doodle and inserts
-result in a Google calender. The approach for now is pretty simple as regex
-are used to extract data from the website directly. A better solution would be
-to read the `xls` file, that can be exported from doodle.com. I also have some
-concerns about the date formats/parsing, right now the approch is very naive,
-and small changes may break the app, again using the `xls` file 'may' resolve
+maja is a small application/script that fetches data from a
+[Doodle](http://doodle.com) and inserts the entries into a Google calender.
+The approach is pretty naive for now, as regex are used to extract data
+from the web page. A better solution would be to read the `xls` file, that
+can be exported from doodle.com. I also have some concerns about the date
+formats/parsing. As mentioned, right now the approch is very naive and small
+changes may break the application, using the `xls` file 'may' also resolve
 this issue.
 
-This code is work in progress, feel free to use it, but do not expect it to
-work ;)
+Example of a run:  
+
+```
+(env)➜ Maja git:(master) ✗ python maja.py
+1   16:00-21:30  Siv
+2   16:00-21:30  Charlotte
+6   16:00-21:30  Carina & Ida Bo
+7   16:00-21:30  Malte & Rasmus
+8   16:00-21:30  Maria, Christen, Janich & Lotten (16.15)
+9   16:00-21:30  Emilie, Patricia, Paulina & Simone
+11  13:00-17:00  Charlotte, Lasse Bo, Sigrid O & Christen
+12  16:00-21:30  Anna, Sophie & Anna Bo
+13  16:00-21:30  Luna, Jonas & Patricia
+14  16:00-21:30  Simon, Sigrid O, Jonas & Kristoffer
+15  16:00-21:30  Mathias, Simone & Anders
+16  16:00-21:30  Sophie, Marta, Emilie & Ida Bo
+20  16:00-21:30  Siv, Ida H & Katrine
+21  16:00-21:30  Luna, Anna Bo, Maria & Kristoffer
+22  16:00-21:30  Maja, Simon, Lotten (16.15) & Janich
+23  16:00-21:30  Rasmus & Stine
+25  13:00-17:00  Paulina, Anna, Stine & Therese
+26  16:00-21:30  Lasse Bo & Therese
+27  16:00-21:30  Marta, Katrine & Carina
+28  16:00-21:30  Malte
+30  16:00-21:30  Mathias, Anders & Ida H
+
+Should I continue? (y/n):
+```
 
 # Run it
 You need a file called `client_secrets.json` containing a
--- maja.py
+++ maja.py
@@ -1,55 +1,105 @@
+from __future__ import absolute_import
+from random import shuffle
+
 import requests
 import re
 import json
 from collections import defaultdict
 import sys
-from datetime import date
+import datetime
 from oauth2client import client
 from googleapiclient import sample_tools
+from collections import namedtuple
+import argparse
+import httplib2
+import os
+
+from googleapiclient import discovery
+from oauth2client import client
+from oauth2client import file
+from oauth2client import tools
+from itertools import groupby
+
+__author__ = 'jonas.brunsgaard@gmail.com (Jonas Brunsgaard)'
 
 
-def parse_date(datestring):
-    m, d, y = map(int, datestring[3:].split('/'))
-    return date(y + 2000, m, d)
+class TGEntryCollector(object):
+    open_ = '16:00', '21:30'
+    open_saturday = '13:00', '17:00'
+
+    def __init__(self, url):
+        self.url = url
+        self.data = self._fetch_json(self.url)
+
+    @staticmethod
+    def _fetch_json(url):
+        resp = requests.get(url)
+        pattern = '.*extend\(true, doodleJS.data, ({"poll":.*})\);'
+        match = re.search(pattern, resp.text)
+        return json.loads(match.groups()[0])
+
+    @staticmethod
+    def parse_date(datestring):
+        m, d, y = map(int, datestring[3:].split('/'))
+        return datetime.date(2000+y, m, d)
+
+    def __iter__(self):
+        Entry = namedtuple('Entry', ['date', 'name'])
+        dates = [self.parse_date(d) for d in self.data['poll']['optionsText']]
+        participants = self.data['poll']['participants']
+        for p in participants:
+            mapping = zip(dates, p['preferences'])
+            dates_filtered = (date for (date, char) in mapping if char == 'y')
+            for date in dates_filtered:
+                yield Entry(date, p['name'].title())
+
+    def shifts(self):
+        Shift = namedtuple('Shift', ['date', 'text', 'start', 'end'])
+        entries = sorted(self)
+        for date, sub_iter in groupby(entries, key=lambda t: t[0]):
+            names = [e.name for e in sub_iter]
+            shuffle(names)
+            text = ' &'.join(', '.join(names).rsplit(',', 1))
+            start, end = self.open_
+            # If Saturday
+            if date.weekday() == 5:
+                start, end = self.open_saturday
+            yield Shift(date, text, start, end)
+
+if __name__ == "__main__":
+    url = 'http://doodle.com/4qeiacg36ppi3h92'
+    url = 'http://doodle.com/6y3xv3mc9z9trq6w'
+    shifts = list(TGEntryCollector(url).shifts())
+    storage = file.Storage('calendar' + '.dat')
+    credentials = storage.get()
+    http = credentials.authorize(http=httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+
+    #  flow = client.flow_from_clientsecrets(
+    #      'client_secrets.json',
+    #      scope='https://www.googleapis.com/auth/calendar')
+
+    events = []
+    for date, text, start, end in shifts:
+        data = {
+            'summary': text,
+            'start': {'dateTime': '{}T{}:00.000+02:00'.format(date, start)},
+            'end': {'dateTime': '{}T{}:00.000+02:00'.format(date, end)},
+        }
+        e = service.events().insert(calendarId='primary', body=data)
+        print('{:<3} {}-{:<6} {}'.format(date.day, start, end, text))
+
+    while True:
+        proceed = raw_input('\nShould I continue? (y/n): ')
+        if proceed in ['y', 'n']:
+            break
+
+    if proceed == 'y':
+        for e in events:
+            pass
+            # e.execute()
+        print('Done!')
+    else:
+        print('Aborted!')
 
 
-url = 'http://doodle.com/4qeiacg36ppi3h92'
-resp = requests.get(url)
-
-pattern = '.*extend\(true, doodleJS.data, ({"poll":.*})\);'
-match = re.search(pattern, resp.text)
-
-data = json.loads(match.groups()[0])
-
-options = data['poll']['optionsText']
-options = [parse_date(d) for d in options]
-
-dates_from_s = lambda s: [d for (d, p) in zip(options, s) if p == 'y']
-d = {e['name']: dates_from_s(e['preferences']) for e in data['poll']['participants']}
-
-res = defaultdict(list)
-for k, v in d.iteritems():
-    for date in v:
-        res[date].append(k)
-
-
-
-#TODO: Replace init wth proper solution
-service, flags = sample_tools.init(
-    sys.argv, 'calendar', 'v3', __doc__, __file__,
-    scope='https://www.googleapis.com/auth/calendar')
-
-#TODO: start/end time saterdays
-for date, names in res.iteritems():
-
-  event = {
-    'summary': ' &'.join(', '.join(names).rsplit(',', 1)) ,
-    'start': {
-            'dateTime': str(date)+ 'T16:00:00.000+02:00',
-            },
-    'end': {
-            'dateTime': str(date) + 'T21:30:00.000+02:00',
-            },
-  }
-
-  e = service.events().insert(calendarId='primary', body=event).execute()
